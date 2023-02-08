7f74f4ee879a13d9774570438334571d384d8f35
YEC <isz06188@naver.com>
2023-02-08 22:38:22 +0900

    TEST4

---
+++ src/main/java/com/example/spring_study_0/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.spring_study_0;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private int age;
+}
