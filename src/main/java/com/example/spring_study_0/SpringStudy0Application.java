e7753e2f033d304c63f0418e2904808641893298
YEC <isz06188@naver.com>
2023-02-08 21:37:58 +0900

    TEST

---
+++ src/main/java/com/example/spring_study_0/SpringStudy0Application.java
@@ -0,0 +1,22 @@
+package com.example.spring_study_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@RestController
+
+public class SpringStudy0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudy0Application.class, args);
+    }
+    @GetMapping( value="/")
+    public String test(){
+        return "testas";
+    }
+
+}
---
+++ src/test/java/com/example/spring_study_0/SpringStudy0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_study_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
